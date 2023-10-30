c619745d03efb21e93b9bd6cae2d6f3a2759a7f8
Gashi-JR <1940404766@qq.com>
2023-10-30 13:18:05 +0800

    style:首页布局

---
+++ smartcampus/unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const _sfc_main = {
+  __name: "index",
+  setup(__props) {
+    common_vendor.ref("hallo");
+    common_vendor.onLoad(() => {
+      console.log(11);
+    });
+    return (_ctx, _cache) => {
+      return {};
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "G:/fronter/STUDY/UNIAPP/smartcampus/smartcampus/pages/index/index.vue"]]);
+wx.createPage(MiniProgramPage);
