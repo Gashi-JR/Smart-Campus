0f39006d64e1550b9fe13e2d6b5fb07874f549b0
Gashi-JR <1940404766@qq.com>
2023-10-31 17:04:28 +0800

    style:首页布局完成

--- smartcampus/pages/index/index.vue
+++ smartcampus/pages/index/index.vue
@@ -7,7 +7,6 @@
 			<text class="title">林桂花江大学</text>
 		</view>
 		<view class="profile">
-
 			<image src="../../static/logo.png" mode="widthFix"
 				style="width: 110rpx;height: 110rpx;border-radius: 50%;margin-right: 30rpx;"></image>
 			<view style="display: flex;flex-direction: column;justify-content: space-between;">
@@ -16,7 +15,6 @@
 				</view>
 				<text style="color: white;font-size: 25rpx;">登录查看更多内容</text>
 			</view>
-
 		</view>
 
 		<image class="water" src="/static/图图 水.png"></image>
@@ -27,20 +25,39 @@
 		</view>
 
 		<view class="feature">
-			<view style="width: 110rpx;height: 110rpx;border-radius: 10rpx;background-color:rgb(57,122,255) ;">
-			</view>
-			<view style="height: 80rpx;width: 4rpx;background-color:rgb(210,210,210);">
+			<view class="feature_item">
+				<view class="feature_icon"
+					style="width: 110rpx;height: 110rpx;border-radius: 10rpx;background-color:rgb(57,122,255) ;margin-bottom: 5vh;">
+					<image class="location" src="/static/icons/闪电.svg"></image>
+				</view>
+
+				用电查询
 
 			</view>
-			<view style="width: 110rpx;height: 110rpx;border-radius: 50%;background-color:rgb(255,139,43) ;">
+
+			<view style="height: 80rpx;width: 4rpx;background-color:rgb(210,210,210);margin-top: 1vh;">
 
 			</view>
-			<view style="height: 80rpx;width: 4rpx;background-color:rgb(210,210,210);">
+			<view class="feature_item">
+				<view class="feature_icon"
+					style="width: 110rpx;height: 110rpx;border-radius: 50%;background-color:rgb(255,139,43) ;margin-bottom: 5vh;">
+
+					<image class="location" src="/static/icons/纸飞机.svg"></image>
+				</view>
+				校园取物
+			</view>
+
+			<view style="height: 80rpx;width: 4rpx;background-color:rgb(210,210,210);margin-top: 1vh;">
 
 			</view>
-			<view style="width: 110rpx;height: 110rpx;border-radius: 10rpx;background-color:rgb(57,122,255) ;">
-
+			<view class="feature_item">
+				<view class="feature_icon"
+					style="width: 110rpx;height: 110rpx;border-radius: 10rpx;background-color:rgb(57,122,255) ;margin-bottom: 5vh;">
+					<image class="location" src="/static/icons/无需安装.svg"></image>
+				</view>
+				网上保修
 			</view>
+
 		</view>
 	</view>
 
@@ -63,12 +80,12 @@
 <style lang="scss">
 	.content {
 		position: relative;
-height:1220rpx ;
+		height: 100vh;
 
 		.logo {
 			position: absolute;
 			width: 100%;
-			height: 350rpx;
+			height: 29%;
 			border-bottom-left-radius: 55rpx;
 			border-bottom-right-radius: 55rpx;
 			z-index: -1;
@@ -99,7 +116,7 @@ height:1220rpx ;
 
 		.profile {
 			position: absolute;
-			top: 100rpx;
+			top: 7%;
 			display: flex;
 			padding-left: 50rpx;
 		}
@@ -111,9 +128,9 @@ height:1220rpx ;
 		.water {
 			position: absolute;
 			border-radius: 40rpx;
-			height: 300rpx;
+			height: 25%;
 			width: 550rpx;
-			top: 270rpx;
+			top: 22%;
 			box-shadow: 0 10rpx 0 0 rgb(141, 177, 252);
 			left: 100rpx;
 		}
@@ -123,7 +140,7 @@ height:1220rpx ;
 			display: flex;
 			justify-content: center;
 			align-items: center;
-			top: 680rpx;
+			top: 57%;
 			left: 50rpx;
 		}
 
@@ -131,12 +148,26 @@ height:1220rpx ;
 
 			position: absolute;
 			left: 10%;
-			//top: 900rpx;
-			bottom: 0;
+			top: 70%;
 			display: flex;
 			width: 80%;
 			justify-content: space-around;
-			align-items: center;
+			align-items: flex-start;
+
+			.feature_item {
+				font-size: 25rpx;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+
+				.feature_icon {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				}
+
+			}
 		}
 
 	}
