4d366a59c9838a06f451797e27247eb599524434
Gashi-JR <1940404766@qq.com>
2023-11-14 20:22:01 +0800

    chore：优化界面和代码

--- smartcampus/unpackage/dist/dev/mp-weixin/wxcomponents/vant-weapp/dist/common/color.js
+++ smartcampus/unpackage/dist/dev/mp-weixin/wxcomponents/vant-weapp/dist/common/color.js
@@ -1,3 +1,7 @@
-"use strict";
-const WHITE = "#fff";
-exports.WHITE = WHITE;
+export const RED = '#ee0a24';
+export const BLUE = '#1989fa';
+export const WHITE = '#fff';
+export const GREEN = '#07c160';
+export const ORANGE = '#ff976a';
+export const GRAY = '#323233';
+export const GRAY_DARK = '#969799';
